51add3dcff15baca448daf3f190e46843cb7f71f
MilkMan90 <mlkaufman747@gmail.com>
2016-12-21 15:45:02 -0700

    reroue

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ import {BrowserRouter, Match, Miss} from 'react-router'
 const Root = ()=>{
   console.log("WOO HOO =) heroku woo!!");
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <div>
-        <h1>WOOOOOOO :-)</h1>
         <Header />
         <Nav />
         <main>
