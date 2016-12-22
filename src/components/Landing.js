72599956ebd3390f3f6046e77cd50e19ebbaccb1
MilkMan90 <mlkaufman747@gmail.com>
2016-12-21 18:06:41 -0700

    added transition, and more styles

---
+++ src/components/Landing.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import turinglogo from '../imgs/turing-logo.png'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group' // ES6
+import '../style/index.css';
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div className="Landing">
+        <ReactCSSTransitionGroup
+        transitionName="landing-animation"
+        transitionAppear={true}
+        transitionAppearTimeout={2200}>
+          <h2>Welcome to Dashboard.</h2>
+          <img className="landing-logo" src={turinglogo}/>
+        </ReactCSSTransitionGroup>
+
+      </div>
+    );
+  }
+}
+
+export default Landing;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router'
+import turinglogo from '../imgs/turing-logowheel.png'
 
 const Nav = () => {
   return (
     <div className="Navigation">
+      <Link className="turing-logo-link" to="/"><img className="turing-logo" src={turinglogo}/></Link>
       <ul>
         <li><Link to="/"> Home </Link></li>
         <li><Link to="/enrollment">Enrollments</Link></li>
